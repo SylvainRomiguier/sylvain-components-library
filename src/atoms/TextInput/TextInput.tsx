1fd716970dd3894a77b3419991ca9f4614e17aeb
Sylvain Romiguier <sylvain.romiguier@gmail.com>
2021-08-06 12:07:44 +0200

    theme implementation

--- src/atoms/TextInput/TextInput.tsx
+++ src/atoms/TextInput/TextInput.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text12pxBold } from "../typography/Typography";
+import styles from "./textInput.module.css";
 
 export interface TextInputProps {
   label: string;
@@ -13,14 +14,15 @@ export const TextInput: React.FC<TextInputProps> = ({
   onChange,
 }) => {
   return (
-    <>
-      <label htmlFor={label}><Text12pxBold>{label}</Text12pxBold></label>
+    <div className={`${styles.form__group} ${styles.field}`}>
+      <label htmlFor={label} className={styles.form__label}><Text12pxBold>{label}</Text12pxBold></label>
       <input
         type="text"
         id={label}
         value={value}
         onChange={(e) => onChange(e.currentTarget.value)}
+        className={styles.form__field}
       />
-    </>
+    </div>
   );
 };
--- src/atoms/containers/Fillet/Fillet.tsx
+++ src/atoms/containers/Fillet/Fillet.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import styles from "./fillet.module.css";
-import colors from "../../colors/colors";
 
 export const Fillet: React.FC = ({ children }) => {
   return (
-    <div className={styles.container} style={{ borderColor: colors.primary }}>
+    <div className={styles.container}>
       {children}
     </div>
   );
