83b1b797e282afb59d6e41ae5f1244e07366d748
Sylvain Romiguier <sylvain.romiguier@gmail.com>
2021-08-07 12:59:13 +0200

    implementing TextInput

--- src/atoms/TextInput/TextInput.tsx
+++ src/atoms/TextInput/TextInput.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { RefObject } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useRef } from "react";
 import { Text12pxBold } from "../typography/Typography";
 import styles from "./textInput.module.css";
 
@@ -13,15 +16,38 @@ export const TextInput: React.FC<TextInputProps> = ({
   value,
   onChange,
 }) => {
+  const [labelState, setLabelState] = useState(styles.form__label_hide);
+  const [placeHolder, setPlaceHolder] = useState(label);
+
+  const onFocus = () => {
+    setLabelState(styles.form__label_show);
+    setPlaceHolder("");
+  };
+
+  const onBlur = () => {
+    setPlaceHolder(label);
+    console.log("value : ", value);
+    if (value === undefined || value === "") {
+      setLabelState(styles.form__label_hide);
+    } else {
+      setLabelState(styles.form__label_show_blur);
+    }
+  };
+
   return (
     <div className={`${styles.form__group} ${styles.field}`}>
-      <label htmlFor={label} className={styles.form__label}><Text12pxBold>{label}</Text12pxBold></label>
+      <label htmlFor={label} className={labelState}>
+        {label}
+      </label>
       <input
         type="text"
         id={label}
         value={value}
         onChange={(e) => onChange(e.currentTarget.value)}
         className={styles.form__field}
+        placeholder={placeHolder}
+        onFocus={onFocus}
+        onBlur={onBlur}
       />
     </div>
   );
